b5b26ed25afdd47054a24c61dcf7916db2a038c6
masterpin <16904082+MasterPin@users.noreply.github.com>
2024-03-16 17:41:11 +0100

    auslagerung der wetter api für übersichtlichkeit

--- src/main.go
+++ src/main.go
@@ -1,61 +1,14 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"github.com/chris-dot-exe/AwesomeLog"
-	"io/ioutil"
-	"net/http"
-	"os"
-)
-
-type HourlyUnits struct {
-	Time          string `json:"time"`
-	Temp          string `json:"temperature_2m"`
-	ApperentTemp  string `json:"apparent_temperature"`
-	Precipitation string `json:"precipitation"`
-}
-
-type WeatherData struct {
-	Latitude             float64     `json:"latitude"`
-	Longitude            float64     `json:"longitude"`
-	GenerationTimeMS     float64     `json:"generationtime_ms"`
-	UTCOffsetSeconds     int         `json:"utc_offset_seconds"`
-	Timezone             string      `json:"timezone"`
-	TimezoneAbbreviation string      `json:"timezone_abbreviation"`
-	Elevation            float64     `json:"elevation"`
-	HourlyUnits          HourlyUnits `json:"hourly_units"`
-	Hourly               struct {
-		Time          []string  `json:"time"`
-		Temp          []float64 `json:"temperature_2m"`
-		ApperentTemp  []float64 `json:"apparent_temperature"`
-		Precipitation []float64 `json:"precipitation"`
-	} `json:"hourly"`
-}
-
 func main() {
-	resp, err := http.Get("https://api.open-meteo.com/v1/dwd-icon?latitude=49.0069&longitude=8.4037&hourly=temperature_2m,apparent_temperature,precipitation&timezone=Europe%2FBerlin")
+	//	todo: make config that is beeing read in by gonfig
+	//	todo: call the weather and put data into struct
+	//	todo: implement template file that gets that data and puts it on a website
 
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+	GetWeather()
 
-	respData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var weatherData WeatherData
-
-	err = json.Unmarshal(respData, &weatherData)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	log.PrettyPrint(weatherData.Hourly.Time)
-	log.PrettyPrint(weatherData.Hourly.Temp)
-	log.PrettyPrint(weatherData.Hourly.ApperentTemp)
-	log.PrettyPrint(weatherData.Hourly.Precipitation)
-
+	//for { //ever
+	//	GetWeather()
+	//	time.Sleep(5 * time.Second)
+	//}
 }
